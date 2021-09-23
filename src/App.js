035d4a97b2946e78900c55ba0af64597be6c6f35
OptimusJc <optimus.prime.jc@gmail.com>
2021-09-23 16:47:59 +0300

    Added the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './header/Header';
 
 
+
 function App() {
   return (
     <div>
--- src/header/Header.js
+++ src/header/Header.js
@@ -5,19 +5,21 @@ import { NavDropdown } from 'react-bootstrap'
 import Form  from "react-bootstrap/Form"
 import { FormControl } from 'react-bootstrap'
 import logo from "../../src/logo.png"
+import "./Header.css"
 
-const Header=()=>(
+const Header = () => (
 
-    <Navbar bg="light" expand="lg">
+  <div className="container">
+  <Navbar className="bg-dark navbar-dark" expand="lg">
   <Navbar.Brand href="#">
-  <img src={logo}  alt="logo" />
-     
+  <img src={logo}  alt="logo" style={{ maxWidth: '250px'}}/>
+    
   </Navbar.Brand>
   <Navbar.Toggle aria-controls="navbarScroll" />
   <Navbar.Collapse id="navbarScroll">
     <Nav
       className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
+      style={{ maxHeight: '100px'}}
       navbarScroll
     >
       <Nav.Link href="#action1">Home</Nav.Link>
@@ -30,17 +32,23 @@ const Header=()=>(
         <NavDropdown.Item href="#action5">Television</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    <Form className="d-flex">
+    <Form className="d-flex" style={{marginLeft: '70px', marginRight: '10px'}}>
       <FormControl
         type="search"
         placeholder="Search"
         className="mr-2"
         aria-label="Search"
+
+        style={{ maxWidth: '200px'}}
       />
       <Button variant="outline-success">Search</Button>
     </Form>
+
+    <Nav.Link href="#action2" className=" btn-primary">Sign Up</Nav.Link>
+    <Nav.Link href="#action2">Log In</Nav.Link>
   </Navbar.Collapse>
 </Navbar>
+</div>
 
 )
 
