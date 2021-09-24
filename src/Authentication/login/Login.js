9545373e31629442741be03a3979a798f8a5e1f9
OptimusJc <optimus.prime.jc@gmail.com>
2021-09-24 09:44:37 +0300

    Added login

--- src/Authentication/login/Login.js
+++ src/Authentication/login/Login.js
@@ -1,7 +1,30 @@
+import Form from 'react-bootstrap/Form'
+import FormGroup from 'react-bootstrap/FormGroup'
+
+
 const Login=()=>{
     return(
         <div>
-            hello
+            <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
         </div>
     )
 }
