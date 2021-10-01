dedb01f5efd17ab60c7b310d7fc85c0b5140019f
OptimusJc <optimus.prime.jc@gmail.com>
2021-10-01 09:05:07 +0300

    not important

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,92 +1,85 @@
 import { Link } from "@reach/router";
 import { ListGroup } from "react-bootstrap";
 
-import { faFacebookF, faYoutube } from '@fortawesome/free-brands-svg-icons' 
+import { faFacebookF, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div className="container fixed-bottom"> 
-        <footer className="page-footer font-small pt-4">
-    <div className="container-fluid text-center text-md-left">
-        <div className="row">
-            <div className="col-md-3 mt-md-0 mt-3">
-                <h5 className="text-uppercase">BCU Media International</h5>
-                <p>We are a move of God</p>
-            </div>
+    return (
+        <div className="container-fluid">
+            <footer className="page-footer font-small pt-4">
+                <div className="container-fluid text-center text-md-left">
+                    <div className="row">
+                        <div className="col-md-3 mt-md-0 mt-3">
+                            <h5 className="text-uppercase">
+                                BCU Media International
+                            </h5>
+                            <p>We are a move of God</p>
+                        </div>
 
-            <hr className="clearfix w-100 d-md-none pb-0"/>
+                        <hr className="clearfix w-100 d-md-none pb-0" />
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                      <Link to="/podcasts">Podcasts</Link>
-                    </li>
-                    <li>
-                      <Link to="/videos">Videos</Link>
-                    </li>
-                </ul>
-            </div>
+                        <div className="col-md-3 mb-md-0 mb-3">
+                            <h5 className="text-uppercase">Links</h5>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/podcasts">Podcasts</Link>
+                                </li>
+                                <li>
+                                    <Link to="/videos">Videos</Link>
+                                </li>
+                            </ul>
+                        </div>
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Channels</h5>
-                <ul className="list-unstyled">
-                  <li>
-                      <Link to="/radio">Radio</Link>
-                    </li>
-                    <li>
-                      <Link to="/television">Television</Link>
-                    </li>
-                </ul>
-            </div>
+                        <div className="col-md-3 mb-md-0 mb-3">
+                            <h5 className="text-uppercase">Channels</h5>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <Link to="/radio">Radio</Link>
+                                </li>
+                                <li>
+                                    <Link to="/television">Television</Link>
+                                </li>
+                            </ul>
+                        </div>
 
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Social Media</h5>
-                {/* <ul className="list-unstyled d-inline">
-                  <li>
-                      <Link to="">
-                      <FontAwesomeIcon icon={ faFacebookF} />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <FontAwesomeIcon icon={ faYoutube} />
-                      </Link>
-                    </li>
-                </ul> */}
+                        <div className="col-md-3 mb-md-0 mb-3">
+                            <h5 className="text-uppercase">Social Media</h5>
 
-        {/* NOTE: Fix the _blank issue */}
-        <ListGroup horizontal  >
-        <ListGroup.Item variant="primary" className="list_group">
-        <a href="https://www.facebook.com/profile.php?id=100008233095156"  >
-                      <FontAwesomeIcon icon={ faFacebookF} />
-                      </a>
-        </ListGroup.Item>
-        <ListGroup.Item variant="primary" className="list_group">
-        <a href="https://www.youtube.com/channel/UC32x9S-LGduGd9Qbc4RAsBQ">
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
+                            {/* NOTE: Fix the _blank issue */}
+                            <ListGroup horizontal>
+                                <ListGroup.Item
+                                    variant="primary"
+                                    className="list_group"
+                                >
+                                    <a href="https://www.facebook.com/profile.php?id=100008233095156">
+                                        <FontAwesomeIcon icon={faFacebookF} />
+                                    </a>
+                                </ListGroup.Item>
+                                <ListGroup.Item
+                                    variant="primary"
+                                    className="list_group"
+                                >
+                                    <a href="https://www.youtube.com/channel/UC32x9S-LGduGd9Qbc4RAsBQ">
+                                        <FontAwesomeIcon icon={faYoutube} />
+                                    </a>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </div>
+                    </div>
+                </div>
 
-        </ListGroup.Item>
-        
-</ListGroup>
-            </div>
+                <div className="footer-copyright text-center py-3">
+                    © 2021 Copyright: BCU-International
+                </div>
+            </footer>
         </div>
-    </div>
-
-    <div className="footer-copyright text-center py-3">© 2021 Copyright:
-        BCU-International
-    </div>
-
-</footer>
-    </div>
-  );
-}
+    );
+};
 
 export default Footer;
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,63 +1,130 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from "react-bootstrap/Nav"
-import Button  from 'react-bootstrap/Button'
-import { NavDropdown } from 'react-bootstrap'
-import Form  from "react-bootstrap/Form"
-import { FormControl } from 'react-bootstrap'
-import logo from "../logo.png"
-import "./Header.css"
-import { Link } from '@reach/router';
-
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import { NavDropdown } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { FormControl } from "react-bootstrap";
+import logo from "../images/bcu-logo.png";
+import { Link } from "@reach/router";
+import "./Header.css";
 
 const Header = () => (
+    <div className="container-fluid ">
+        {/* <Navbar variant="dark" className="dark navbar-expand-lg" expand="md">
+            <Navbar.Brand as={Link} to="/">
+                <img src={logo} alt="logo" className="navbar-brand" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll" className="d-flex">
+                <Nav
+                    className="mr-auto my-2 my-lg-0 flex-grow-1"
+                    style={{ maxHeight: "100px" }}
+                    navbarScroll
+                >
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/podcasts">
+                        Podcasts
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/videos">
+                        Videos
+                    </Nav.Link>
 
-  <div className="container">
-  <Navbar className="bg-dark navbar-dark" expand="md">
-  <Navbar.Brand href="#">
-  <img src={logo}  alt="logo" style={{ maxWidth: '250px'}}/>
-    
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px'}}
-      navbarScroll
-    >
-  
-      <Nav.Link as={Link} to="/">Home</Nav.Link>
-      <Nav.Link as={Link} to="/podcasts">Podcasts</Nav.Link>
-      <Nav.Link as={Link} to="/videos">Videos</Nav.Link>
+                    <NavDropdown title="Channels" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">
+                            Radio
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Television
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
 
-      <NavDropdown title="Channels" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Radio</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Television</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form className="d-flex" style={{marginLeft: '70px', marginRight: '10px'}}>
-      <FormControl
-        type="search"
-        placeholder="Search"
-        className="mr-2"
-        aria-label="Search"
+                <Form className="d-flex " style={{ marginLeft: "30px" }}>
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="mr-2"
+                        aria-label="Search"
+                        style={{ maxWidth: "200px" }}
+                    />
+                </Form>
 
-        style={{ maxWidth: '200px'}}
-      />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+                <Nav.Link as={Link} to="/signup">
+                    <Button variant="warning" className="btn-warning">
+                        Sign up
+                    </Button>
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                    <Button
+                        variant="outline-warning"
+                        className="btnOutlineWarning"
+                    >
+                        Login
+                    </Button>
+                </Nav.Link>
+            </Navbar.Collapse>
+        </Navbar> */}
 
-    <Nav.Link as={Link} to="/signup">
-      <Button variant="danger"> Sign Up</Button>
-    </Nav.Link>
-    <Nav.Link as={Link} to="/login" >Log In</Nav.Link>
-  </Navbar.Collapse>
-</Navbar>
-</div>
+        <Navbar variant="dark" className="dark" expand="lg">
+            <Navbar.Brand href="#">
+                <img src={logo} alt="logo" className="navbar-brand" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="mr-auto my-2 my-lg-0 flex-grow-1"
+                    style={{ maxHeight: "100px" }}
+                    navbarScroll
+                >
+                    <Nav.Link as={Link} to="/">
+                        Home
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/podcasts">
+                        Podcasts
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/videos">
+                        Videos
+                    </Nav.Link>
 
-)
+                    <NavDropdown title="Channels" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">
+                            Radio
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Television
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="mr-2"
+                        aria-label="Search"
+                    />
+                </Form>
+
+                <Nav.Link as={Link} to="/signup">
+                    <Button variant="warning" className="btn-warning">
+                        Sign up
+                    </Button>
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                    <Button
+                        variant="outline-warning"
+                        className="btnOutlineWarning"
+                    >
+                        Login
+                    </Button>
+                </Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    </div>
+);
 
 export default Header;
