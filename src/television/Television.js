5d600d3c5b147bfb97d5075a547226f8eba48475
OptimusJc <optimus.prime.jc@gmail.com>
2021-10-13 10:51:46 +0300

    Fixed Tv component styling for small screens

--- src/television/Television.js
+++ src/television/Television.js
@@ -1,14 +1,16 @@
 import Card from "react-bootstrap/Card";
 import ReactPlayer from "react-player";
 
+import './Television.css';
+
 const Television = () => {
     return (
         <div>
-            <Card className=" text-white">
+            <Card className=" text-white"  >
                 <Card.Title className="text-center">
                     NESBIT TELEVISION
                 </Card.Title>
-                <ReactPlayer
+                <ReactPlayer className="video-container"
                     url={[
                         "https://www.youtube.com/watch?v=QuB5JoX063Q",
                         "https://www.youtube.com/watch?v=jNgP6d9HraI",
