c1a0d8012b8de9e78ffa2411e88ce984c47b915f
OptimusJc <optimus.prime.jc@gmail.com>
2021-11-24 17:20:07 +0300

    Finished the mobile section together with the header and main of the tablet view

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,6 +2,10 @@ import { Row, Col } from "react-bootstrap";
 import Television from "../television/Television";
 import Videos from "../videos/Videos";
 import "./Home.css";
+import ReactPlayer from "react-player/youtube";
+
+// dummy images 
+import image from "../images/bcu_logo.webp"
 
 const Home = () => {
     return (
@@ -33,12 +37,66 @@ const Home = () => {
             </Row>
             </div>
             <h2 className="heading-2">
-                Body of Christ International Videos on Demand
+                Body of Christ United International Videos on Demand
             </h2>
             {/* <Videos /> */}
-            <ul>
+            <ul className="video_list">
                 <li>
-                  <img src="../images/bcu_logo.webp" />
+                    <span>001</span>
+                    <ReactPlayer className="video_image" url="https://www.youtube.com/watch?v=be6i4m2GeBE" />
+                    <span className="live">live</span>
+                    <p>First Video Title</p>
+                </li>
+                <li>
+                    <span>002</span>
+                    <ReactPlayer className="video_image" url="https://www.youtube.com/watch?v=jX5EEgA6upQ" />
+                    <span className="live">live</span>
+                    <p>Second Video Title</p>
+                </li>
+                <li>
+                    <span>003</span>
+                    <img src={image} 
+                    alt="placeholder image"
+                    className="video_image"
+                    />
+                    <span className="live">live</span>
+                    <p>Third Video Title</p>
+                </li>
+                <li>
+                    <span>004</span>
+                  <img src={image} 
+                    alt="placeholder image"
+                    className="video_image"
+                    />
+                    <span className="live">live</span>
+                    <p>Fourth Video Title</p>
+                </li>
+                <li>
+                    <span>005</span>
+                  <img src={image} 
+                    alt="placeholder image"
+                    className="video_image"
+                    />
+                    <span className="live">live</span>
+                    <p>Fifth Video Title</p>
+                </li>
+                <li>
+                    <span>006</span>
+                  <img src={image} 
+                    alt="placeholder image"
+                    className="video_image"
+                    />
+                    <span className="live">live</span>
+                    <p>Sixth Video Title</p>
+                </li>
+                <li>
+                    <span>007</span>
+                  <img src={image} 
+                    alt="placeholder image"
+                    className="video_image"
+                    />
+                    <span className="live">live</span>
+                    <p>Seventh Video Title</p>
                 </li>
             </ul>
         </div>
--- src/television/Television.js
+++ src/television/Television.js
@@ -2,19 +2,17 @@ import Card from "react-bootstrap/Card";
 import ReactPlayer from "react-player";
 
 import './Television.css';
+import sunrise_video from "/home/optimus/Desktop/Development/bcu-home-clone/src/images/sunrise.mp4";
 
 const Television = () => {
     return (
         <div className="television">
             <Card className=" text-white"  >
-                <Card.Title className="text-center">
+                <Card.Title className="text-center mb-4 mt-0">
                     NESBIT TELEVISION
                 </Card.Title>
-                <ReactPlayer className="video-container"
-                    url={
-                        
-                        "https://video.wixstatic.com/video/889011_185a9a86819e493fa94c43a9ac817769/480p/mp4/file.mp4"
-                    }
+                <ReactPlayer playing="true" loop className="video-container"
+                    url={ sunrise_video }
                 />
             </Card>
             <div className="spacing"></div>
