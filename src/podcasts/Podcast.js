97c72874dee8d381ea2ac432d99c9041bf251730
OptimusJc <otienoj.cruz@outlook.com>
2022-06-11 20:57:02 +0300

    fix: Add correct styling for the podcasts section for presentation

--- src/podcasts/Podcast.js
+++ src/podcasts/Podcast.js
@@ -1,4 +1,5 @@
 import "./podcast.modules.css";
+import baby_image from "../images/pexels-nappy-935944.jpg";
 
 const Podcast = ({ title, author, date, path, doc_id }) => {
     return (
@@ -13,17 +14,19 @@ const Podcast = ({ title, author, date, path, doc_id }) => {
                             document_url: { path },
                         }}
                     >
-                        <img
-                            src="https://via.placeholder.com/300x200"
-                            alt=""
-                            className="featureImage"
-                        />
-                        <div>
+                        <div className="image_cont">
+                            <img
+                                src={baby_image}
+                                alt=""
+                                className="featureImage"
+                            />
+                        </div>
+                        <div className="feature_details">
                             <h6 className="h6">{author}</h6>
                             <p>{title}</p>
                             <span>
                                 {/* <BiTime className={["clock"]} /> */}
-                                {date}
+                                {/* {date} */}
                             </span>
                         </div>
                     </a>
