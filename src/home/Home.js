d073a4edf8af4e477282f33017c14d36f34994fd
OptimusJc <otienoj.cruz@outlook.com>
2022-06-22 16:26:19 +0300

    fix: Add correct styling and structure to the header section

--- src/home/Home.js
+++ src/home/Home.js
@@ -10,102 +10,103 @@ const Home = () => {
     return (
         <div className="home">
             <div className="live-section">
-                <p>live</p>
-                <h3>
+                <p>
+                    <span>live</span>
                     The body of christ united global international ministries
-                </h3>
+                </p>
                 <button className="btn btn-primary"> share this page</button>
             </div>
-            <div className="column">
-                <Row>
-                    <Col className="tv-box">
-                        <Television />
-                    </Col>
-                </Row>
-
-                <Row className="coming-soon">
-                    <Col>
-                        <p className="display-6">
-                            BCU Television
-                            <sub className="lead">coming soon...</sub>
-                        </p>
-                    </Col>
-                </Row>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="tv-box">
+                            <Television />
+                        </div>
+                        <div>
+                            <p className="display-6">
+                                BCU Television
+                                <sub className="lead">coming soon...</sub>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <h2 className="heading-2">
+                            Body of Christ United International Videos on Demand
+                        </h2>
+                        {/* <Videos /> */}
+                        <ul className="video_list">
+                            <li>
+                                <span>001</span>
+                                <ReactPlayer
+                                    className="video_image"
+                                    url="https://www.youtube.com/watch?v=be6i4m2GeBE"
+                                />
+                                <span className="live">live</span>
+                                <p>First Video Title</p>
+                            </li>
+                            <li>
+                                <span>002</span>
+                                <ReactPlayer
+                                    className="video_image"
+                                    url="https://www.youtube.com/watch?v=jX5EEgA6upQ"
+                                />
+                                <span className="live">live</span>
+                                <p>Second Video Title</p>
+                            </li>
+                            <li>
+                                <span>003</span>
+                                <img
+                                    src={image}
+                                    alt="placeholder image"
+                                    className="video_image"
+                                />
+                                <span className="live">live</span>
+                                <p>Third Video Title</p>
+                            </li>
+                            <li>
+                                <span>004</span>
+                                <img
+                                    src={image}
+                                    alt="placeholder image"
+                                    className="video_image"
+                                />
+                                <span className="live">live</span>
+                                <p>Fourth Video Title</p>
+                            </li>
+                            <li>
+                                <span>005</span>
+                                <img
+                                    src={image}
+                                    alt="placeholder image"
+                                    className="video_image"
+                                />
+                                <span className="live">live</span>
+                                <p>Fifth Video Title</p>
+                            </li>
+                            <li>
+                                <span>006</span>
+                                <img
+                                    src={image}
+                                    alt="placeholder image"
+                                    className="video_image"
+                                />
+                                <span className="live">live</span>
+                                <p>Sixth Video Title</p>
+                            </li>
+                            <li>
+                                <span>007</span>
+                                <img
+                                    src={image}
+                                    alt="placeholder image"
+                                    className="video_image"
+                                />
+                                <span className="live">live</span>
+                                <p>Seventh Video Title</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <h2 className="heading-2">
-                Body of Christ United International Videos on Demand
-            </h2>
-            {/* <Videos /> */}
-            <ul className="video_list">
-                <li>
-                    <span>001</span>
-                    <ReactPlayer
-                        className="video_image"
-                        url="https://www.youtube.com/watch?v=be6i4m2GeBE"
-                    />
-                    <span className="live">live</span>
-                    <p>First Video Title</p>
-                </li>
-                <li>
-                    <span>002</span>
-                    <ReactPlayer
-                        className="video_image"
-                        url="https://www.youtube.com/watch?v=jX5EEgA6upQ"
-                    />
-                    <span className="live">live</span>
-                    <p>Second Video Title</p>
-                </li>
-                <li>
-                    <span>003</span>
-                    <img
-                        src={image}
-                        alt="placeholder image"
-                        className="video_image"
-                    />
-                    <span className="live">live</span>
-                    <p>Third Video Title</p>
-                </li>
-                <li>
-                    <span>004</span>
-                    <img
-                        src={image}
-                        alt="placeholder image"
-                        className="video_image"
-                    />
-                    <span className="live">live</span>
-                    <p>Fourth Video Title</p>
-                </li>
-                <li>
-                    <span>005</span>
-                    <img
-                        src={image}
-                        alt="placeholder image"
-                        className="video_image"
-                    />
-                    <span className="live">live</span>
-                    <p>Fifth Video Title</p>
-                </li>
-                <li>
-                    <span>006</span>
-                    <img
-                        src={image}
-                        alt="placeholder image"
-                        className="video_image"
-                    />
-                    <span className="live">live</span>
-                    <p>Sixth Video Title</p>
-                </li>
-                <li>
-                    <span>007</span>
-                    <img
-                        src={image}
-                        alt="placeholder image"
-                        className="video_image"
-                    />
-                    <span className="live">live</span>
-                    <p>Seventh Video Title</p>
-                </li>
-            </ul>
         </div>
     );
 };
